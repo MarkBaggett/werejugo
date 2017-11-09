7ef55b411b441076061953c8ac2eb6d3f4835c23
Mark Baggett <lo127001@gmail.com>
2017-11-09 14:41:27 -0500

    Update to Beta code

--- README.md
+++ README.md
@@ -1,18 +1,25 @@
 # werejugo
 
-Version 0.1 - prebeta
+Version 0.5 - beta
 
-Identifies physical locations where a laptop has been based upon wireless profiles and wireless data recorded in event logs.
+Identifies physical locations where a laptop has been based upon multiple forensics artifacts on host. Including
+    - Wireless Profiles in SOFTWARE registry hive
+    - Wireless SSID from WLAN_AUTOCONFIG Application Event logs
+    - *Wireless Diagnostic Event ID 6100 in SYSTEM.EVTX 
+    - **Wireless Profile Names from SRUM.DAT
+
+* Google Disabled this API in September of 2017.  I'll have to rewrite this to use a different API which will most likely require you to obtain API keys.
+
+** Not yet implimented.
 
 Resolving geolocation of the Wireless profiles stored in the SOFTWARE registry hive is done with Wigle.net's API.  You have to obtain a wigle.net API username and password.   The API username and password are differnt than your normal API username and password.   After creating a free account go to http://wigle.net/account and click the "SHOW MY TOKEN".  Then use API NAME (username) and API TOKEN (password).  These are passed as the -u and -p arguments
 
 If you don't havea wigle.net account you can still use the Event Log based resolution.  The event log resolution looks for Microsoft Diagnotic Code 6100 event logs.  It finds Wifi information in the event log and geolocates it.
 
+Example 1 - Analyze SOFTWARE registry hive and WLAN AUTO CONFIG files,do not geolocate with Wigle and create default output file called werejugo_output.xlsx: 
+werejugo.exe -r SOFTWARE -w Microsoft-Windows-WLAN-AutoConfig.evtx 
 
-Example 1 - Assume default locations of artifacts and create a spreadsheet called results.xlsx: 
-werejugo.exe -i z:\C: -u "wigleapiuser" -p "wigleapipassword" -o results.xlsx
-
-Example 2 - Geolocate any 6100 event logs entries in the System event log.
+Example 2 - Geolocate any 6100 event logs entries in the System event log and create a file called eventsonly.xlsx
 werejugo.exe -e z:\c\windows\system32\Winevt\Logs\System.evtx -o eventsonly.xlsx
 
 Example 3 - Geolocate the Wireless APs in multiple software registry hive files all starting with the word SOFTWARE in the current directory.
@@ -21,9 +28,10 @@ werejugo.exe -r SOFTWARE*.* -u "wigleapiuser" -p "wigleapipassword" -o results.x
 (Note: attemping to analyze either the System.evtx or SOFTWARE registry hive on live systems probably wont work because of file locks)
 
 
-usage: werejugo.exe [-h] [--IMAGE_MOUNT_POINT IMAGE_MOUNT_POINT]
+usage: werejugo.py [-h] [--IMAGE_MOUNT_POINT IMAGE_MOUNT_POINT]
                     [--SOFTWARE_REGISTRY SOFTWARE_REGISTRY]
-                    [--SYSTEM_EVENTS SYSTEM_EVENTS] [--OUTPUT OUTPUT]
+                    [--SYSTEM_EVENTS SYSTEM_EVENTS]
+                    [--WLAN_EVENTS WLAN_EVENTS] [--OUTPUT OUTPUT]
                     [--WIGLE_USER WIGLE_USER] [--WIGLE_PASS WIGLE_PASS]
 
 Given a directory of SOFTWARE registry hives it will enumerate all the
@@ -41,12 +49,16 @@ optional arguments:
                         Location of System Event Log specified. (Support
                         Wildcards) Example:
                         c:\windows\system32\Winevt\Logs\System.evtx)
+  --WLAN_EVENTS WLAN_EVENTS, -w WLAN_EVENTS
+                        Location of WLAN Autoconfig Event Log specified.
+                        (Support Wildcards) Example:
+                        c:\windows\system32\Winevt\Logs\Microsoft-Windows-
+                        WLAN-AutoConfig perational.evtx)
   --OUTPUT OUTPUT, -o OUTPUT
                         Path and filename of the XLSX file to create.
   --WIGLE_USER WIGLE_USER, -u WIGLE_USER
-                        Wigle.net API Username. Required for geolocating
-                        SOFTWARE_REGISTRY. See https://wigle.net/account
+                        Wigle.net API Username. Required for geolocating See
+                        https://wigle.net/account
   --WIGLE_PASS WIGLE_PASS, -p WIGLE_PASS
-                        Wigle.net API Password. Required for geolocating
-                        SOFTWARE_REGISTRY. See https://wigle.net/account
-
+                        Wigle.net API Password. Required for geolocating See
+                        https://wigle.net/account
--- werejugo.py
+++ werejugo.py
@@ -13,6 +13,12 @@ import Evtx.Evtx
 import Evtx.Views
 import Evtx.Nodes
 import os
+import itertools
+
+try:
+    input=raw_input
+except:
+    pass
 
 def xml_records(filename):
     #Code based on https://raw.githubusercontent.com/williballenthin/python-evtx/master/scripts/evtx_filter_records
@@ -45,7 +51,13 @@ def google_cheat(networks):
     url = 'https://maps.googleapis.com/maps/api/browserlocation/json?browser=firefox&sensor=true'
     for eachnetwork in networks:
         url += '&wifi=mac:{0}|ssid:{2}|ss:{1}'.format(*eachnetwork)
-    x = requests.get(url).json()
+    webresp = requests.get(url)
+    if webresp.status_code == 200:
+        x = webresp.json()
+    else:
+        print("URL: {0}".format(url))
+        print(webresp.status_code, webresp.reason, webresp.content)
+        return("Not Found", "Not Found")
     accuracy =  "Accuracy of %s square meters" % (x['accuracy'])
     geourl =  "http://maps.google.com/maps?q=%.9f,%.9f&z=15" % (x['location']['lat'], x['location']['lng'])
     return (accuracy, geourl)
@@ -68,26 +80,78 @@ def parse_6100(path_to_evtx):
             results.append((str(SystemTime), EventDescription, accuracy, url))
     return results
 
+def parse_wlan_autoconfig(path_to_evtx):
+    results = []
+    Connect_Events = [8001 ] 
+    Disconnect_Events = [8003] 
+    Failed_Events = [8002, 11002]
+    Still_Connected  = [11004, 11005, 11010, 11006, 12011, 12012, 12013]
+    for node, err in xml_records(path_to_evtx):
+        if err is not None:
+            continue
+        EventMetadata = get_child(node, "System")
+        EventData = get_child(node, "EventData")
+        SystemTime = get_child(EventMetadata, "TimeCreated").attrib
+        EventDescription = "\n".join(["{0}:{1}".format(x.values()[0],str(x.text)) for x in EventData])
+        EventID = int(get_child(EventMetadata, "EventID").text)
+        if EventID in Connect_Events:
+            eventtype = "Connect"
+        elif EventID in Disconnect_Events:
+            eventtype = "Disconnect"
+        elif EventID in Failed_Events:
+            eventtype = "Failed Connection"
+        else:
+            continue
+        ProfileName = re.search(r"SSID:(\S+)", EventDescription).group(1)
+        BSSID = profile_table.get(ProfileName, "MAC-UNKNOWN")
+        mapurl = "Unable to determine MAC for SSID"
+        if BSSID != "MAC-UNKNOWN":
+            mapurl = "Unable to find MAC on Wigle"
+            result = wigle_search(netid=BSSID)
+            if len(result['results']):
+                for eachresult in result['results']:
+                    mapurl =  "http://maps.google.com/maps?q=%.9f,%.9f&z=15" % (eachresult['trilat'], eachresult['trilong'])
+        results.append((eventtype, BSSID, ProfileName, SystemTime['SystemTime'], mapurl ))
+    return results
+
 def get_reg_value(path_to_file, path, key):
     rh = Registry.Registry(path_to_file)
     rk = rh.open(path)
     return rk.value(key).value()
 
+wigle_cache={}
 def wigle_search(**kwargs):
     #Lookup like this  wigle_search(netid="ff:ff:ff:ff:ff")
+    if kwargs['netid'] in wigle_cache:
+        print("From Cache")
+        return wigle_cache.get(kwargs['netid'])
     url = "https://api.wigle.net/api/v2/network/search"
-    return requests.get(url, auth = (options.WIGLE_USER,options.WIGLE_PASS), params = kwargs).json()
+    if not options.WIGLE_USER or not options.WIGLE_PASS:
+            return {'results':[{'trilat':0, 'trilong': 0}]}
+    try:
+        webresp = requests.get(url, auth = (options.WIGLE_USER,options.WIGLE_PASS), params = kwargs)
+        if webresp.status_code != 200:
+            print("*"*25,"There was an error from Wigle. {0}".format(webresp.reason))
+        result = webresp.json()
+        wigle_cache[kwargs['netid']] = result
+        if result['success']==False:
+            raise(Exception("*"*25, "Wigle Request Failed {0}".format(result['message'])))
+    except Exception as e:
+        cont = input("Bad things happened while talking to Wigle. {0}, {1}  Continue? [Y/N] ".format(webresp.reason, str(e)))
+        if cont.lower()=="n":
+            raise(Exception("Wigle connection error. {0} {1} {2}".format(webresp.status_code,webresp.reason,str(e))))
+    return result
 
 def reg_date(dateblob):
     weekday = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
     year,month,day,date,hour,minute,sec,micro = struct.unpack('<8H', dateblob)
-    dt =  "%s, %02d/%02d/%04d %02d:%02d:%02d.%s"  % (weekday[day], month,date,year,hour,minute,sec,micro)
+    dt = "%s, %02d/%02d/%04d %02d:%02d:%02d.%s" % (weekday[day],month,date,year,hour,minute,sec,micro)
     dtp = datetime.datetime.strptime(dt,"%A, %m/%d/%Y %H:%M:%S.%f")
     return dtp
 
 def get_profile_info(reg_handle, ProfileGuid):
     nametypes = {'47':"Wireless", "06":"Wired", "17":"Broadband"}
-    guid = "Microsoft\Windows NT\CurrentVersion\NetworkList\Profiles\%s" % (ProfileGuid)
+    guid = b"Microsoft\Windows NT\CurrentVersion\NetworkList\Profiles\%s" % (ProfileGuid)
     regkey = reg_handle.open(guid)
     NameType ="%02x"  % regkey.value("NameType").value()
     nettype = nametypes.get(str(NameType), "Unknown Type"+str(NameType))
@@ -96,8 +160,9 @@ def get_profile_info(reg_handle, ProfileGuid):
     return nettype, FirstConnect, LastConnect
 
 def network_history(reg_handle):
+    global profile_table
     reg_results = []
-    for eachsubkey in reg_handle.open("Microsoft\Windows NT\CurrentVersion\NetworkList\Signatures\Unmanaged").subkeys():
+    for eachsubkey in reg_handle.open(b"Microsoft\Windows NT\CurrentVersion\NetworkList\Signatures\Unmanaged").subkeys():
         DefaultGatewayMac = eachsubkey.value("DefaultGatewayMac").value()
         BSSID = ':'.join(DefaultGatewayMac.encode("HEX")[i:i+2] for i in range(0,12,2))
         Description = eachsubkey.value("Description").value()
@@ -108,14 +173,15 @@ def network_history(reg_handle):
         mapurl="Not found"
         #print "TYPE:",nettype, " SSID:",SSID, "Description:", Description, "DNS SUFFIX", DnsSuffix, "MAC ADDRESS:", BSSID
         if nettype=="Wireless":
-            result= wigle_search(netid=BSSID)
+            result = wigle_search(netid=BSSID)
             if len(result['results']):
                 for eachresult in result['results']:
-                    print "Found: ", SSID, BSSID
-                    print "First connected: ",first
-                    print "Last Connected : ",last  
+                    #print "Found: ", SSID, BSSID
+                    #print "First connected: ",first
+                    #print "Last Connected : ",last  
                     mapurl =  "http://maps.google.com/maps?q=%.9f,%.9f&z=15" % (eachresult['trilat'], eachresult['trilong'])
             #print google_cheat(BSSID, SSID)
+            profile_table[SSID] = BSSID
         reg_results.append((BSSID,SSID,Description,DnsSuffix,nettype,first,last,mapurl))
     return reg_results
 
@@ -127,7 +193,7 @@ def reg_basic_info():
     ControlSet = "ControlSet%03d" % (which_control_set)
     comp_name = get_reg_value("SYSTEM",ControlSet+"\Control\ComputerName\ComputerName","ComputerName")
     time_zone = get_reg_value("SYSTEM",ControlSet+"\Control\TimeZoneInformation","DayLightName")
-    print osversion, which_control_set, comp_name, time_zone
+    print(osversion, which_control_set, comp_name, time_zone)
 
 def reg_interface_info():
     #Retrieve local area network connections
@@ -136,19 +202,20 @@ def reg_interface_info():
     interfaces = rh.open(ControlSet+"\Services\Tcpip\Parameters\Interfaces").subkeys()
     for eachinterface in interfaces:
         try:
-            print eachinterface.value("DhcpIPAddress").value()
-            print eachinterface.value("DhcpDomain").value()
+            print(eachinterface.value("DhcpIPAddress").value())
+            print(eachinterface.value("DhcpDomain").value())
         except:
             pass
 
 parser = argparse.ArgumentParser(description="Given a directory of SOFTWARE registry hives it will enumerate all the wireless.")
 parser.add_argument("--IMAGE_MOUNT_POINT","-i",default='', help ="Geolocate all sources searching the SYSTEM volume of this mounted Windows Images . example: z:\image\C:")
 #parser.add_argument("--recurse", help = "Recursively, Exhaustively search the entire drive for any usable file instead of looking in default locatoins.", action="store_true", )
-parser.add_argument("--SOFTWARE_REGISTRY","-r", default="\Windows\System32\Config\SOFTWARE", help ="Location of the SOFTWARE registry keys. (Supports Wildcards) example: c:\windows\system32\config\SYSTEM*")
-parser.add_argument("--SYSTEM_EVENTS","-e", default="\Windows\System32\Winevt\Logs\System.evtx", help ="Location of System Event Log specified. (Support Wildcards) Example: c:\windows\system32\Winevt\Logs\System.evtx)")
-parser.add_argument("--OUTPUT","-o", default="wireless_output.xlsx", help ="Path and filename of the XLSX file to create.")
-parser.add_argument("--WIGLE_USER", "-u", help="Wigle.net API Username.  Required for geolocating SOFTWARE_REGISTRY.  See https://wigle.net/account")
-parser.add_argument("--WIGLE_PASS", "-p", help="Wigle.net API Password.  Required for geolocating SOFTWARE_REGISTRY.  See https://wigle.net/account")
+parser.add_argument("--SOFTWARE_REGISTRY","-r", default="", help ="Location of the SOFTWARE registry keys. (Supports Wildcards) example: c:\windows\system32\config\SYSTEM*")
+parser.add_argument("--SYSTEM_EVENTS","-e", default="", help ="Location of System Event Log specified. (Support Wildcards) Example: c:\windows\system32\Winevt\Logs\System.evtx)")
+parser.add_argument("--WLAN_EVENTS","-w", default="", help ="Location of WLAN Autoconfig Event Log specified. (Support Wildcards) Example: c:\windows\system32\Winevt\Logs\Microsoft-Windows-WLAN-AutoConfig perational.evtx)")
+parser.add_argument("--OUTPUT","-o", default="werejugo_output.xlsx", help ="Path and filename of the XLSX file to create.")
+parser.add_argument("--WIGLE_USER", "-u", help="Wigle.net API Username.  Required for geolocating  See https://wigle.net/account")
+parser.add_argument("--WIGLE_PASS", "-p", help="Wigle.net API Password.  Required for geolocating  See https://wigle.net/account")
 
 options = parser.parse_args()
 
@@ -156,7 +223,8 @@ options = parser.parse_args()
 #    print("Exhaustive search not implemented yet.")
 
 reg_files = os.path.join(options.IMAGE_MOUNT_POINT,options.SOFTWARE_REGISTRY) 
-evt_files = os.path.join(options.IMAGE_MOUNT_POINT,options.SYSTEM_EVENTS) 
+evt_files = os.path.join(options.IMAGE_MOUNT_POINT,options.SYSTEM_EVENTS)
+wlan_evts_files = os.path.join(options.IMAGE_MOUNT_POINT,options.WLAN_EVENTS)
 if "*" in reg_files:
     reg_files = glob.glob(reg_files)
 else:
@@ -165,48 +233,105 @@ if "*" in evt_files:
     evt_files = glob.glob(evt_files)
 else:
     evt_files = [evt_files]
+if "*" in wlan_evts_files:
+    wlan_evts_files = glob.glob(wlan_evts_files)
+else:
+    wlan_evts_files = [wlan_evts_files]
 
-if reg_files:
-    if not options.WIGLE_USER or not options.WIGLE_PASS:
-        print("A Wigle API username and password is required for Registry geolocation resolution.  Obtain one https://wigle.net/account.")
-        sys.exit(1)
+
+if not options.WIGLE_USER or not options.WIGLE_PASS:
+        print("\nA Wigle API username and password is required for geolocation resolution.  Obtain one https://wigle.net/account.")
+        print("All Locations will be 0,0.\n\n")
 
 #Retrieve Wifi history
 print("Enumerating Wireless History and GPS Locations")
 target_wb = openpyxl.Workbook()
 
-#Do the wireless Data sheet
-xls_sheet = target_wb.create_sheet(title="Wireless Data")
-columns = ['BSSID', 'SSID','Description', 'DNSSufix', 'Type', 'First Seen', 'Last Seen', 'Geolocation URL']
+#Create a Consolidated Timeline tab in the spreadsheet
+timeline_sheet = target_wb.create_sheet(title="Consolidated Timeline")
+columns = ['Date','BSSID', 'SSID','Geolocation URL']
 for column, value in enumerate(columns):
-    xls_sheet.cell(row = 1, column = column+1).value = value
-for eachfile in reg_files:
-    try:
-        reg_handle = Registry.Registry(eachfile)
-        reg_handle.open("Microsoft\Windows NT\CurrentVersion\NetworkList\Signatures\Unmanaged")
-    except Exception as e:
-        print("Unable to process file {0} : {1} ".format(eachfile,e))
-        continue
-    reg_data = network_history(reg_handle)
-    for row, row_data in enumerate(reg_data):
-        for column, value in enumerate(row_data):
-            xls_sheet.cell(row = row+2, column = column+1).value = value
+    timeline_sheet.cell(row = 1, column = column+1).value = value
+
+
+#Do the wireless Data sheet
+if options.SOFTWARE_REGISTRY:
+    print("Enumerating Wireless Profiles in the Registry...")
+    xls_sheet = target_wb.create_sheet(title="Wireless Data")
+    columns = ['BSSID', 'SSID','Description', 'DNSSufix', 'Type', 'First Seen', 'Last Seen', 'Geolocation URL']
+    profile_table = {}
+    for column, value in enumerate(columns):
+        xls_sheet.cell(row = 1, column = column+1).value = value
+    for eachfile in reg_files:
+        try:
+            reg_handle = Registry.Registry(eachfile)
+            reg_handle.open(b"Microsoft\Windows NT\CurrentVersion\NetworkList\Signatures\Unmanaged")
+        except Exception as e:
+            print("Unable to process file {0} : {1} ".format(eachfile,e))
+        else:
+            reg_data = network_history(reg_handle)
+            for row, row_data in enumerate(reg_data):
+                for column, value in enumerate(row_data):
+                    xls_sheet.cell(row = row+2, column = column+1).value = value
+                if row_data[7].startswith("http"):
+                    timeline_sheet.append([row_data[5], row_data[0], row_data[1],row_data[7]])
+                    timeline_sheet.append([row_data[6], row_data[0], row_data[1],row_data[7]])
+        try:
+            reg_handle = Registry.Registry(eachfile)
+            reg_handle.open(b"Microsoft\Windows NT\CurrentVersion\NetworkList\Signatures\Managed")
+        except Exception as e:
+            print("Unable to process file {0} : {1} ".format(eachfile,e))
+        else:
+            reg_data = network_history(reg_handle)
+            profile_table[reg_data[1]] = reg_data[0]
+            for row, row_data in enumerate(reg_data):
+                for column, value in enumerate(row_data):
+                    xls_sheet.cell(row = row+2, column = column+1).value = value
+                if row_data[7].startswith("http"):
+                    timeline_sheet.append([row_data[5], row_data[0], row_data[1],row_data[7]])
+                    timeline_sheet.append([row_data[6], row_data[0], row_data[1],row_data[7]])
+
 
 #Do the Event 6100 sheet
-print("Enumerating Event 6100 data")
-xls_sheet = target_wb.create_sheet(title="Event 6100 Data")
-columns = ['System Time', 'Data','Geolocation Accuracy', 'Geolocation URL']
-for column, value in enumerate(columns):
-    xls_sheet.cell(row = 1, column = column+1).value = value
-for eachfile in evt_files:
-    try:
+if options.SYSTEM_EVENTS:
+    print("Enumerating Event 6100 data from SYSTEM event logs...")
+    print("Google disabled the required API.  This feature is temporarily unavailable.")
+    """xls_sheet = target_wb.create_sheet(title="Event 6100 Data")
+    columns = ['System Time', 'Data','Geolocation Accuracy', 'Geolocation URL']
+    for column, value in enumerate(columns):
+        xls_sheet.cell(row = 1, column = column+1).value = value
+    for eachfile in evt_files:
+        #try:
         evt_data  = parse_6100(eachfile)
-    except Exception as e:
-        print("Unable to process file {0} : {1} ".format(eachfile,e))
-        continue
-    for row, row_data in enumerate(evt_data):
-        for column, value in enumerate(row_data):
-            xls_sheet.cell(row = row+2, column = column+1).value = value
+        #except Exception as e:
+        #    print("Unable to process file {0} : {1} ".format(eachfile,e))
+        #    continue
+        for row, row_data in enumerate(evt_data):
+            for column, value in enumerate(row_data):
+                xls_sheet.cell(row = row+2, column = column+1).value = value
+            if row_data[3].startswith("http"):
+                timeline_sheet.append([row_data[0], row_data[1], "hmm",row_data[3]])
+    """
+
+#Do WLAN_AUTOCONFIG
+if options.WLAN_EVENTS:
+    print("Enumerating WLAN Autoconfig Log data")
+    xls_sheet = target_wb.create_sheet(title="WLAN Events Data")
+    columns = ['ACTION', 'BSSID','SSID', 'System Time', "Geolocation URL"]
+    for column, value in enumerate(columns):
+        xls_sheet.cell(row = 1, column = column+1).value = value
+    for eachfile in wlan_evts_files:
+        try:
+            evt_data  = parse_wlan_autoconfig(eachfile)
+        except Exception as e:
+            print("Unable to process file {0} : {1} ".format(eachfile,e))
+            continue
+        for row, row_data in enumerate(evt_data):
+            for column, value in enumerate(row_data):
+                xls_sheet.cell(row = row+2, column = column+1).value = value
+            if row_data[4].startswith("http"):
+                timeline_sheet.append([row_data[3], row_data[1], row_data[2],row_data[4]])
+
 
 firstsheet=target_wb.get_sheet_by_name("Sheet")
 target_wb.remove_sheet(firstsheet)
